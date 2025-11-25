cfd442b4145651001e160b90d0643c1900045d89
daniel <daniel@cohere.com>
2025-11-25 10:10:41 -0500

    add comemnt

--- release/main.go
+++ release/main.go
@@ -5,7 +5,8 @@ import (
 
 	melody "github.com/cohere-ai/melody/release/go-bindings"
 )
-
+// main is a test function to ensure the release build is minimally
+// functional (mostly that it's importable and callable)
 func main() {
     filter := melody.NewFilter()
     fo := filter.WriteDecoded("Hello", nil)
